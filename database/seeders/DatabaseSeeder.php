b65a40fff5c61806405937d5d4c23134f888dfff
Haniyeh9757 <haniyeh.keshvarii@gmail.com>
2025-05-20 14:56:39 +0330

    add relations and seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $this->call([
+            AuthorSeeder::class,
+            PublisherSeeder::class,
+            CategorySeeder::class,
+            BookSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
